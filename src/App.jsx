c574da946e2cba5daceb473cb7b33c09210e91f4
gallardobruno <gallardobruno.ar@gmail.com>
2023-12-20 17:37:12 -0300

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { lazy, Suspense } from 'react'
 import Page404 from './assets/404'
 import { LandingSeller, LandingUser, PostBike} from './containers/index.jsx'
 import { Route, Routes } from 'react-router-dom'
-const TestComponent = lazy(() => import('./TestField/TestComponent.jsx')) 
+import { TestComponent } from './TestField/TestComponent.jsx'
 const SearchPage = lazy(() => import('./assets/SearchPage')) 
 
 
@@ -15,7 +15,7 @@ function App() {
           <Route path='/' element = { <LandingUser companyName={"BiMo reg"}/> } />
           <Route path='/seller' element = { <LandingSeller companyName={"BiMo reg"}/> } />
           <Route path='/post' element = { <PostBike/> } />
-          <Route path='/test' element = {TestComponent} />
+          <Route path='/test' element = { <TestComponent/> } />
           <Route path='/search/:query' element = {SearchPage} />
           <Route path='*' element={<Page404 />} />
         </Routes>
